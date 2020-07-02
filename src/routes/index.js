25262331ff8445c74a44b88d882801f49a05a90f
nayoungKim-hub <rlaskdud59988@gmail.com>
2020-07-02 16:09:17 +0900

    dbTest 삭제

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,12 @@ const { Router } = require('express')
 const router = Router()
 
 const users = require('./users')
-const test = require('./dbTest')
 const flowers = require('./flowers')
 const jwtTest = require('./jwtTest')
 const video = require('./youtube')
+
 /* GET home page. */
 router.use('/users', users)
-router.use('/dbTest', test)
 router.use('/flowers', flowers)
 router.use('/jwtTest', jwtTest)
 router.use('/youtube', video)
